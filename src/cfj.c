7bfc24c9e36bfc284fa398835d868194401a86e6
Siguza <siguza@siguza.net>
2022-07-08 11:36:59 +0200

    Correct copyright year for cfj

--- src/cfj.c
+++ src/cfj.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020 Siguza
+/* Copyright (c) 2019-2021 Siguza
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
